9b6dc0e11d8f595eba04d5482bba269aecb16f7b
shyi06 <songhune0627@gmail.com>
2025-06-08 16:00:07 +0900

    feat: Create main.py for FastAPI

--- api/main.py
+++ api/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+import wandb
 
 
 app = FastAPI()
+
+
+# 모델 로딩
+run = wandb.init()
+artifact = run.use_artifact('jandar-tech/ml-ops-practice2/xgboost:v0', type='model')
+artifact_dir = artifact.download()
+
+
+# 입력 데이터 스키마 정의
+class InputData(BaseModel):
+    weather: str
+    "지역": str
 
 
 @app.get("/")
